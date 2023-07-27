160d12446501fb33dbe530a0140f5f60810c5fef
김해인 <khi762@naver.com>
2023-07-27 18:02:13 +0900

    recommit

--- 0x05/2493.py
+++ 0x05/2493.py
@@ -3,25 +3,32 @@ input = sys.stdin.readline
 
 N = int(input())
 heights = list(map(int, input().split()))
-results = []
+results = [0]
+received = 0
+top = heights[-1]
 
-for i in range(N - 1):
-    top = heights[-1]  # 확인할 탑
-    copyed = heights.copy()  # 탑 리스트를 복사해서
-    receive = False  # 수신 안됨
-    for i in range(len(heights)-1):
-        copyed.pop()         # 복사한 탑을 pop하며 top과 비교
-        if top < copyed[-1]:  # 레이저가 수신되면
-            tower = heights.index(copyed[-1]) + 1  # 몇 번째 탑인지 확인
-            receive = True  # 수신 됨
-            break
-    heights.pop()  # 다음 차례
+
+
+
+
+
+# for i in range(N - 1):
+#     top = heights[-1]  # 확인할 탑
+#     copyed = heights.copy()  # 탑 리스트를 복사해서
+#     receive = False  # 수신 안됨
+#     for i in range(len(heights)-1):
+#         copyed.pop()         # 복사한 탑을 pop하며 top과 비교
+#         if top < copyed[-1]:  # 레이저가 수신되면
+#             tower = heights.index(copyed[-1]) + 1  # 몇 번째 탑인지 확인
+#             receive = True  # 수신 됨
+#             break
+#     heights.pop()  # 다음 차례
         
-    if receive:
-        results.append(tower)
-    else:
-        results.append(0)
+#     if receive:
+#         results.append(tower)
+#     else:
+#         results.append(0)
 
-results.append(0)  # 1번째 탑
-results.reverse()
-print(*results)
+# results.append(0)  # 1번째 탑
+# results.reverse()
+# print(*results)
