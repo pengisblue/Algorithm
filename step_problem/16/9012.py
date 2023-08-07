da0ae54be62bb807865b73f87e2f95f19e9252d2
김해인 <khi762@naver.com>
2023-08-08 00:47:01 +0900

    9012

--- step_problem/16/9012.py
+++ step_problem/16/9012.py
@@ -3,5 +3,24 @@ input = sys.stdin.readline
 
 
 N = int(input())
-for i in range(N):
-    brackets = list(map(int, input()))
+for _ in range(N):
+    brackets = list(input())
+    stack = []
+    VPS = True
+    for i in range(len(brackets)-1):
+        if brackets[i] == '(':
+            stack.append('(')
+        else:
+            if not stack:
+                VPS = False
+                break
+            else:
+                stack.pop()
+
+    if stack:
+        VPS = False
+
+    if VPS:
+        print('YES')
+    else:
+        print('NO')
