9ecb731b072559c82954c0c6ee587332a0e3600b
김해인 <khi762@naver.com>
2023-10-09 16:51:11 +0900

    좌표 정렬하기

--- step_problem/13/11650/sol.py
+++ step_problem/13/11650/sol.py
@@ -1,7 +1,12 @@
+from queue import PriorityQueue
 import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
+que = PriorityQueue()
 N = int(input())
 for i in range(N):
-    x, y = map(int, input().split())
+    que.put(tuple(map(int, input().split())))
+
+for i in range(N):
+    print(*que.get())
